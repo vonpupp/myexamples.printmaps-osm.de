9ed8c27c8ab963953a69ada13bc344879b371ad8
vonpupp <albert@haevas.com>
2019-06-16 19:53:58 +0200

    Add labels (needs review)

--- printmaps-osm.de_sample10_1_50000_scratch_malaga/gridlabels.lua
+++ printmaps-osm.de_sample10_1_50000_scratch_malaga/gridlabels.lua
@@ -16,9 +16,8 @@ version : 2018/12/02
 
 numberOfGridLines = 13
 labelTextIncrement = 0.01
-
 labelPositionIncrement = 19.68
-labelText = 36
+labelText = -4.45
 labelPosition = 27.15
 
 print()
@@ -27,7 +26,7 @@ print("# ---------------------------------")
 print()
 for index = 1, numberOfGridLines, 1 do
     print(string.format("- Style: <TextSymbolizer fontset-name='fontset-0' size='12' fill='dimgray' allow-overlap='true'>'%.2f°'</TextSymbolizer>", labelText))
-    print(string.format("  WellKnownText: POINT(%.2f 5.0)", labelPosition))
+    print(string.format("  WellKnownText: POINT(%.2f 15.5)", labelPosition))
 --    print(string.format("- Style: <TextSymbolizer fontset-name='fontset-0' size='18' fill='dimgray' allow-overlap='true'>'%02d'</TextSymbolizer>", labelText))
 --    print(string.format("  WellKnownText: POINT(%.2f 825.5)", labelPosition))
     print()
@@ -39,8 +38,9 @@ for index = 1, numberOfGridLines, 1 do
 end
 
 numberOfGridLines = 9
+labelTextIncrement = 0.01
 labelPositionIncrement = 19.68
-labelText = -4
+labelText = 36.66
 labelPosition = 27.15
 
 print(string.format("# lat grid values (increment %.2f)", labelPositionIncrement))
@@ -48,7 +48,7 @@ print("# ---------------------------------")
 print()
 for index = 1, numberOfGridLines, 1 do
     print(string.format("- Style: <TextSymbolizer fontset-name='fontset-0' size='12' fill='dimgray' allow-overlap='true'>'%.2f°'</TextSymbolizer>", labelText))
-    print(string.format("  WellKnownText: POINT(15.5 %.2f)", labelPosition))
+    print(string.format("  WellKnownText: POINT(15.0 %.2f)", labelPosition))
 --    print(string.format("- Style: <TextSymbolizer fontset-name='fontset-0' size='18' fill='dimgray' allow-overlap='true'>'%02d'</TextSymbolizer>", labelText))
 --    print(string.format("  WellKnownText: POINT(825.5 %.2f)", labelPosition))
     print()
